2e3a1ab5699948d8fd91f52bec95d06c2a606b47
JoseFreire <jifreire.dev@gmail.com>
2024-12-09 00:31:24 -0300

    Checkpoint: creación de estructura básica

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 # itskool_app
+=======
+# itskool_app
+
+A new Flutter project.
+>>>>>>> e3d814e (Checkpoint: creación de estructura básica)
---
+++ lib/config/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../screens/login_screen.dart';
+import '../screens/home_screen.dart';
+
+class AppRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case '/home':
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  body: Center(child: Text('Página no encontrada')),
+                ));
+    }
+  }
+}
